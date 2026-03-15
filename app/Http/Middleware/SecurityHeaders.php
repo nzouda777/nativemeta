52e86f8107e29f1c3425d86de40feeb1431c3c08
nzouda777 <nzoudarodrigue@gmail.com>
2026-03-15 23:15:05 +0100

    secure disable

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -18,12 +18,12 @@ class SecurityHeaders
 
         $host = $request->getHost();
         $csp = "default-src 'self' https: 'unsafe-inline' 'unsafe-eval'; " .
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://cdn.ngrok.com https://*.ngrok.io https://*.ngrok-free.app; " .
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://cdn.ngrok.com https://*.ngrok.io https://*.ngrok-free.app http://187.124.42.23 https://187.124.42.23/; " .
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://api.fontshare.com https:; " .
-            "font-src 'self' data: https://fonts.gstatic.com https://fonts.bunny.net https://cdn.fontshare.com https://ngrok.com https:; " .
+            "font-src 'self' data: https://fonts.gstatic.com https://fonts.bunny.net https://cdn.fontshare.com https://ngrok.com http://187.124.42.23 https://187.124.42.23 https:; " .
             "img-src 'self' data: https: blob:; " .
-            "connect-src 'self' https://api.stripe.com https://*.ngrok-free.app https://*.ngrok.io ws: wss: {$host}; " .
-            "frame-src 'self' https://js.stripe.com https://www.youtube.com https://player.vimeo.com https://*.ngrok-free.app; " .
+            "connect-src 'self' https://api.stripe.com https://*.ngrok-free.app https://*.ngrok.io https://187.124.42.23 http://187.124.42.23 ws: wss: {$host}; " .
+            "frame-src 'self' https://js.stripe.com https://www.youtube.com https://player.vimeo.com http://187.124.42.23 https://187.124.42.23 https://*.ngrok-free.app; " .
             "media-src 'self' blob: https:;";
 
         $response->headers->set('Content-Security-Policy', $csp);
